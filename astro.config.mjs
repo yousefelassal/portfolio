7fc2b0137e2fa0f335d50f2bc8202230d035bb02
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-22 12:18:29 +0000

    cleanup

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-export default defineConfig({
-  site: 'https://yousefelassal.me',
-  compressHTML: true,
-  integrations: [tailwind(), sitemap()]
-});
