321ab0fadf4c6be75407662de2ac01a7053bd962
Yousef Elassal <yousefsameh24@gmail.com>
2024-06-07 15:56:47 +0300

    refactor: Sort items in rss.xml.ts by date in descending order

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -6,19 +6,28 @@ type Context = {
   site: string
 }
 
+type Entry = {
+  data: {
+    title: string
+    summary: string
+    date: Date
+  }
+  slug: string
+}
+
 export async function GET(context: Context) {
 	const posts = await getCollection("blog")
   const projects = await getCollection("projects")
 
   const items = [...posts, ...projects]
 
-  items.sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
+  items.sort((a:Entry, b:Entry) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
 
   return rss({
     title: SITE.TITLE,
     description: SITE.DESCRIPTION,
     site: context.site,
-    items: items.map((item) => ({
+    items: items.map((item:Entry) => ({
       title: item.data.title,
       description: item.data.summary,
       pubDate: item.data.date,
